03239bf4af0fbe0e9eb3e442c62967d47284672e
Zach M <zatchmdws@gmail.com>
2020-02-23 12:37:21 -0500

    final push

--- Assets/JS/test-script.js
+++ Assets/JS/test-script.js
@@ -44,54 +44,6 @@ function getWeather(city) {
 }
 
 
-//   var cities = [];
-
-//   $("#searchBtn").on("click", function(){
-
-//     var searchBarResult = $("#searchBar").val();
-//     city = $(".searchItem").val(getWeather(searchBarResult));
-
-//     getWeather(city);
-
-
-// cities.push(searchBarResult);
-
-
-//  var leftBtn = $("<button>"+(searchBarResult)+"</button>");
-//  leftBtn.attr("data-city",(searchBarResult));
-//  leftBtn.addClass("searchList");
-
-//  $(".searchHolder").append(leftBtn);
-
-//  localStorage.setItem("city", searchBarResult);
-
-// })
-
-
-//   function getInfo() {
-//       var stayOnScreen = localStorage.getItem("city");
-//       if(stayOnScreen) { 
-
-//      var rememberedButton = $("<button>"+(stayOnScreen)+"</button>");
-//      rememberedButton.attr("data-city",(stayOnScreen));
-//      rememberedButton.addClass("searchList");
-
-//      $(".searchHolder").append(rememberedButton);
-
-//     }
-//   }
-
-// var savedLocations = JSON.parse(localStorage.getItem("oldLocation"));
-
-// if (savedLocations){ 
-//     savedSearch()
-// } else {
-//     savedLocations = []
-// }
-
-
-
-
 var cities = JSON.parse(localStorage.getItem("city"));
 
 if (cities) {
@@ -141,7 +93,8 @@ $(".searchHolder").on("click", "button", function () {
     $(".bigDisplay").empty();
     $(".card-deck").empty();
     $(".fiveDay").empty();
-    
+    $("#right-container").attr("style", "display: block;")
+
 
     var bigT = $(this).attr("data-city");
     var item = $(".searchItem");
@@ -207,7 +160,7 @@ $(".searchHolder").on("click", "button", function () {
 
 function createBottomBlocks() {
 
-     $("#right-container").append($("<h2 class='fiveDay'>5 Day Forecast:</h2>"));
+    $("#right-container").append($("<h2 class='fiveDay'>5 Day Forecast:</h2>"));
 
     var APIkey = "4971bbf933e132e86212d1bc1c100553";
 
@@ -220,112 +173,106 @@ function createBottomBlocks() {
         method: "GET"
     }).then(function (response) {
 
-        
+
 
         for (i = 0; i < 40; i++) {
 
 
 
 
-        console.log(response);
-        var today = new Date();
-        var month = (today.getMonth() + 1) + "/";
-        var day = + today.getDate();
-        var year = + today.getFullYear();
+            console.log(response);
+            var today = new Date();
+            var month = (today.getMonth() + 1) + "/";
+            var day = + today.getDate();
+            var year = + today.getFullYear();
 
 
-        if((response.list[i].dt_txt).includes("12:00:00") === true && i < 10) { 
-            
-            var icon = response.list[i].weather[0].icon;
-            
-            if (response.list[i].weather[0].main !== null) {
-            icon = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon + "@2x.png'>");
-        }
+            if ((response.list[i].dt_txt).includes("12:00:00") === true && i < 10) {
 
-            
-           
+                var icon = response.list[i].weather[0].icon;
 
-            var blueTemp = $("<p class = 'blueTemp'>Temp: " + response.list[i].main.temp + " °F</p>");
-            var blueHum = $("<p class = 'blueHum'>Humidity: " + response.list[i].main.humidity + "%</p>");
-            var blueIcon = $("<h1>"+icon+"</h1>");
-
-            // var blueTemp1 = $("<p class = 'blueTemp-"+ i +"'>" + response.list[i].main.temp + "</p>");
-            // var blueTemp2 = $("<p class = 'blueTemp2' >" + response.list[i].main.temp + "</p>");
-            // var blueTemp3 = $("<p class = 'blueTemp3' >" + response.list[i].main.temp + "</p>");
-            // var blueTemp4 = $("<p class = 'blueTemp4' >" + response.list[i].main.temp + "</p>");
+                if (response.list[i].weather[0].main !== null) {
+                    icon = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon + "@2x.png'>");
+                }
 
 
-          
-        
 
 
-        var blueTitle = $("<h1 class = blueTitle>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
-        blueTitle.text(month + (day + 1) + '/' + year);
-        var blueTitle1 = $("<h1 class = blueTitle1>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
-        blueTitle1.text(month + (day + 2) + '/' + year);
-        var blueTitle2 = $("<h1 class = blueTitle2>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
-        blueTitle2.text(month + (day + 3) + '/' + year);
-        var blueTitle3 = $("<h1 class = blueTitle3>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
-        blueTitle3.text(month + (day + 4) + '/' + year);
-        var blueTitle4 = $("<h1 class = blueTitle4>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
-        blueTitle4.text(month + (day + 5) + '/' + year);
+                var blueTemp = $("<p class = 'blueTemp'>Temp: " + response.list[i].main.temp + " °F</p>");
+                var blueHum = $("<p class = 'blueHum'>Humidity: " + response.list[i].main.humidity + "%</p>");
+                var blueIcon = $("<h1>" + icon + "</h1>");
 
 
-        } else if((response.list[i].dt_txt).includes("12:00:00") === true &&  i > 10 && i < 20) { 
 
-            var icon1 = response.list[i].weather[0].icon;
-            
-            if (response.list[i].weather[0].main !== null) {
-            icon1 = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon1 + "@2x.png'>");
-        }
-            var blueTemp1 = $("<p class = 'blueTemp1'>Temp: " + response.list[i].main.temp + " °F</p>");
-            var blueHum1 = $("<p class = 'blueHum1'>Humidity: " + response.list[i].main.humidity + "%</p>");
-            var blueIcon1 = $("<h1>"+icon1+"</h1>");
 
-        } else if((response.list[i].dt_txt).includes("12:00:00") === true && i > 20 && i < 30) { 
-            var icon2 = response.list[i].weather[0].icon;
-            
-            if (response.list[i].weather[0].main !== null) {
-            icon2 = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon2 + "@2x.png'>");
-        }
 
-            var blueTemp2 = $("<p class = 'blueTemp2' >Temp: " + response.list[i].main.temp + " °F</p>");
-            var blueHum2 = $("<p class = 'blueHum2'>Humidity: " + response.list[i].main.humidity + "%</p>");
-            var blueIcon2 = $("<h1>"+icon2+"</h1>");
+                var blueTitle = $("<h1 class = blueTitle>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
+                blueTitle.text(month + (day + 1) + '/' + year);
+                var blueTitle1 = $("<h1 class = blueTitle1>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
+                blueTitle1.text(month + (day + 2) + '/' + year);
+                var blueTitle2 = $("<h1 class = blueTitle2>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
+                blueTitle2.text(month + (day + 3) + '/' + year);
+                var blueTitle3 = $("<h1 class = blueTitle3>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
+                blueTitle3.text(month + (day + 4) + '/' + year);
+                var blueTitle4 = $("<h1 class = blueTitle4>" + (response.list[i].dt_txt).replace('12:00:00', '') + "</h1>");
+                blueTitle4.text(month + (day + 5) + '/' + year);
 
-        } else if((response.list[i].dt_txt).includes("12:00:00") === true && i >= 30 && i < 34) { 
-            var icon3 = response.list[i].weather[0].icon;
-            
-            if (response.list[i].weather[0].main !== null) {
-            icon3 = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon3 + "@2x.png'>");
-        }
 
-            var blueTemp3 = $("<p class = 'blueTemp3' >Temp: " + response.list[i].main.temp + " °F</p>");
-            var blueHum3 = $("<p class = 'blueHum3'>Humidity: " + response.list[i].main.humidity + "%</p>");
-            var blueIcon3 = $("<h1>"+icon3+"</h1>");
+            } else if ((response.list[i].dt_txt).includes("12:00:00") === true && i > 10 && i < 20) {
 
-        } else if((response.list[i].dt_txt).includes("12:00:00") === true && i > 34) { 
-            var icon4 = response.list[i].weather[0].icon;
-            
-            if (response.list[i].weather[0].main !== null) {
-            icon4 = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon4 + "@2x.png'>");
-        }
+                var icon1 = response.list[i].weather[0].icon;
 
-            var blueTemp4 = $("<p class = 'blueTemp4' >Temp: " + response.list[i].main.temp + " °F</p>");
-            var blueHum4 = $("<p class = 'blueHum4'>Humidity: " + response.list[i].main.humidity + "%</p>");
-            var blueIcon4 = $("<h1>"+icon4+"</h1>");
+                if (response.list[i].weather[0].main !== null) {
+                    icon1 = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon1 + "@2x.png'>");
+                }
+                var blueTemp1 = $("<p class = 'blueTemp1'>Temp: " + response.list[i].main.temp + " °F</p>");
+                var blueHum1 = $("<p class = 'blueHum1'>Humidity: " + response.list[i].main.humidity + "%</p>");
+                var blueIcon1 = $("<h1>" + icon1 + "</h1>");
 
-        }
+            } else if ((response.list[i].dt_txt).includes("12:00:00") === true && i > 20 && i < 30) {
+                var icon2 = response.list[i].weather[0].icon;
 
-    
+                if (response.list[i].weather[0].main !== null) {
+                    icon2 = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon2 + "@2x.png'>");
+                }
 
-        console.log(response.city.name);
-            
+                var blueTemp2 = $("<p class = 'blueTemp2' >Temp: " + response.list[i].main.temp + " °F</p>");
+                var blueHum2 = $("<p class = 'blueHum2'>Humidity: " + response.list[i].main.humidity + "%</p>");
+                var blueIcon2 = $("<h1>" + icon2 + "</h1>");
 
-                // var blueBoxTemp1 = $(".blueBox-0");
-                
-        
-            
+            } else if ((response.list[i].dt_txt).includes("12:00:00") === true && i >= 30 && i < 34) {
+                var icon3 = response.list[i].weather[0].icon;
+
+                if (response.list[i].weather[0].main !== null) {
+                    icon3 = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon3 + "@2x.png'>");
+                }
+
+                var blueTemp3 = $("<p class = 'blueTemp3' >Temp: " + response.list[i].main.temp + " °F</p>");
+                var blueHum3 = $("<p class = 'blueHum3'>Humidity: " + response.list[i].main.humidity + "%</p>");
+                var blueIcon3 = $("<h1>" + icon3 + "</h1>");
+
+            } else if ((response.list[i].dt_txt).includes("12:00:00") === true && i > 34) {
+                var icon4 = response.list[i].weather[0].icon;
+
+                if (response.list[i].weather[0].main !== null) {
+                    icon4 = ("<img id='icon' src='http://openweathermap.org/img/wn/" + icon4 + "@2x.png'>");
+                }
+
+                var blueTemp4 = $("<p class = 'blueTemp4' >Temp: " + response.list[i].main.temp + " °F</p>");
+                var blueHum4 = $("<p class = 'blueHum4'>Humidity: " + response.list[i].main.humidity + "%</p>");
+                var blueIcon4 = $("<h1>" + icon4 + "</h1>");
+
+            }
+
+
+
+            console.log(response.city.name);
+
+
+            // var blueBoxTemp1 = $(".blueBox-0");
+
+
+
 
 
         }
@@ -350,28 +297,17 @@ function createBottomBlocks() {
         blueTitle4.append(blueIcon4);
         blueIcon4.append(blueTemp4);
         blueTemp4.append(blueHum4);
-        
-
-
-
-    //     console.log(blueTemps);
-
-    //    for ( i = 0; i < 5; i++) { 
-    //        $(".blueTemp").addClass("blueTemp-"+i);
-    //        console.log($(".blueTemp"));
-    //    }
 
     })
 
 
 
-    // var newCardDeck = $("<div class='card-deck'</div>");
 
 
     var newCardText = $("<div class='card-text'>Some text inside</p>");
 
 
-    
+
     $("#right-container").append($(".card-deck"));
 
     for (i = 0; i < 5; i++) {
@@ -392,7 +328,7 @@ function getUV() {
     var lat = $(".lat").text();
     var lon = $(".lon").text();
 
-    console.log(lat,lon);
+    console.log(lat, lon);
 
 
 
@@ -404,7 +340,7 @@ function getUV() {
 
         console.log(response)
 
-        
+
 
         var newUV = $("<p class = newUV>UV Index: <button type='button' class='btn btn-danger'>" + (response[0].value) + "</button></p>");
         $(".bigDisplay").append(newUV);
